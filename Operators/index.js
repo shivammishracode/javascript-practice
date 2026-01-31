a1a5106647ab05716e91b3bb5a427e2bcf4c00f2
Shivam Mishra <jimishrashivam9891@gmail.com>
2026-01-31 14:25:59 +0530

    Added Operators topic

---
+++ Operators/index.js
@@ -0,0 +1,37 @@
+// arithmatic operators , comparison , logical , assignment , unary , ternary
+
+//arithmatic operator
+
+console.log(2+1)  //3
+console.log("2"+1) //"21"
+console.log(2-1)  // 1
+console.log("2"-1) // 1
+console.log(2*1)  //2
+console.log(2**4)  //16
+console.log(10/2)  // 5
+console.log(10%2)  // 0
+
+
+// assignment operator and comparison operator
+
+let a=5
+// a+=5
+// a-=5
+// a*=5
+// a/=5
+let b=5
+let s="5"
+console.log(a===s) //check dataype plus value
+console.log(a==s)// check only value
+console.log(a>6) // false
+console.log(a<3) // false
+console.log(a==5) // true
+let m=10
+console.log(a>=m) //false
+console.log(m<=a) // false
+console.log(a==m) // false
+console.log(a<=m) // true
+
+
+
+
